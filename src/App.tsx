95dcd50f330ddffc2ad43b1a7674605064716507
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-07-25 01:41:58 -1200

    added the home, users and products pages in the router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import Products from './pages/products/Products';
+import Users from './pages/users/Users';
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home />
+      ),
+    },
+    {
+      path: "products",
+      element: <Products />
+    },
+    {
+      path: "users",
+      element: <Users />
+    },
+  ]);
   return (
-    <div className="App">
-
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
